25ff70206a1be5cf7c6630665988171b8b3524ca
VERAGUBER <VERA2@UW.EDU>
2023-05-04 11:16:55 -0700

    fixed formatting

--- import.py
+++ import.py
@@ -6,22 +6,29 @@ mongoClient = MongoClient("mongodb://localhost/pokemon")
 pokemonDB = mongoClient['pokemondb']
 pokemonColl = pokemonDB['pokemon_data']
 
+# result = pokemonColl.delete_many({})
+
 connection = sqlite3.connect('pokemon.sqlite')
 pokemonCursor = connection.cursor()
 pokemon_data = pokemonCursor.execute(
-    "SELECT p.name, p.pokedex_number, p.hp, p.attack, p.defense, p.speed, p.sp_attack, p.sp_defense, (t2.type1 || ' ' || t2.type2) AS types FROM pokemon AS p JOIN pokemon_types_view AS t2 ON p.name = t2.name").fetchall()
+    "SELECT p.name, p.pokedex_number, p.hp, p.attack, p.defense, p.speed, p.sp_attack, p.sp_defense FROM pokemon p LIMIT 10").fetchall()
 
 for poke in pokemon_data:
+    print(poke)
     ability_lst = []
     abilities = pokemonCursor.execute(
         "SELECT a.name FROM ability a JOIN pokemon_abilities p ON a.id = p.ability_id WHERE p.pokemon_id=?", (poke[1],)).fetchall()
     for ability in abilities:
         new = ability[0].strip("',")
         ability_lst.append(new)
+    types_lst = []
+    types = pokemonCursor.execute(
+        "SELECT type1, type2 FROM pokemon_types_view p WHERE p.name=?", (poke[0],)).fetchall()
+
     pokemon_object = {
         "name": poke[0],
         "pokedex_number": poke[1],
-        "types": [poke[8]],
+        "types": [types[0][0], types[0][1]],
         "hp": poke[2],
         "attack": poke[3],
         "defense": poke[4],
